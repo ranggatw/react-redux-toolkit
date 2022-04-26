d302f6afcb1ea9c3ce71bc6662d5241994c8d499
ranggatw <rangga.public@gmail.com>
2022-04-26 18:43:28 +0700

    Add new function restocked cake

--- index.js
+++ index.js
@@ -2,33 +2,46 @@ const redux = require('redux')
 const createStore = redux.createStore
 
 const CAKE_ORDERED = "CAKE_ORDERED"
+const CAKE_RESTOKED = "CAKE_RESTOKED";
 
 // Action creators
 function orderCake() {
   return {
     type: CAKE_ORDERED,
-    quantity: 1
-  }
+    payload: 1,
+  };
+}
+
+function restockCake(qty = 1) {
+  return {
+    type: CAKE_RESTOKED,
+    payload: qty,
+  };
 }
 
 const initialState = {
   numOfCakes: 10,
-}
+};
 
 // Reducer
 // (previousState, action) = newState
 
 const reducer = (state = initialState, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case CAKE_ORDERED:
       return {
         ...state,
-        numOfCakes: state.numOfCakes - 1
-      }
-    default: 
-    return state
+        numOfCakes: state.numOfCakes - 1,
+      };
+    case CAKE_RESTOKED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes + action.payload,
+      };
+    default:
+      return state;
   }
-}
+};
 
 // Store
 const store = createStore(reducer)
@@ -39,7 +52,6 @@ const unsubscribe = store.subscribe(() => console.log("update state", store.getS
 store.dispatch(orderCake())
 store.dispatch(orderCake())
 store.dispatch(orderCake())
-store.dispatch(orderCake())
-store.dispatch(orderCake())
+store.dispatch(restockCake(3));
 
 unsubscribe()
